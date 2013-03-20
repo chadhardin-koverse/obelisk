52495978c9ebdddf862a34ac17b3db46bea7bfe7
Chad Hardin <chadhardin@koverse.com>
2013-03-19 22:27:58 -0700

    Created Variables object

---
+++ mr-prioritize-replicate/src/main/java/cehardin/nsu/mr/prioritize/replicate/id/DataBlockAddress.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cehardin.nsu.mr.prioritize.replicate.id;
+
+import com.google.common.base.Objects;
+
+/**
+ *
+ * @author Chad
+ */
+public class DataBlockAddress {
+	private final NodeAddress nodeAddress;
+	private final DataBlockId dataBlockId;
+
+	public DataBlockAddress(NodeAddress nodeAddress, DataBlockId dataBlockId) {
+		this.nodeAddress = nodeAddress;
+		this.dataBlockId = dataBlockId;
+	}
+
+	public NodeAddress getNodeAddress() {
+		return nodeAddress;
+	}
+
+	public DataBlockId getDataBlockId() {
+		return dataBlockId;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(nodeAddress, dataBlockId);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) {
+			return true;
+		}
+		else if(o instanceof DataBlockAddress) {
+			DataBlockAddress other = DataBlockAddress.class.cast(o);
+			return Objects.equal(nodeAddress, other.nodeAddress) && Objects.equal(dataBlockId, other.dataBlockId);
+		}
+		else {
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return Objects.toStringHelper(getClass()).add("nodeAddress", nodeAddress).add("dataBlockId", dataBlockId).toString();
+	}
+}
---
+++ mr-prioritize-replicate/src/main/java/cehardin/nsu/mr/prioritize/replicate/id/TaskId.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cehardin.nsu.mr.prioritize.replicate.id;
+
+/**
+ *
+ * @author Chad
+ */
+public class TaskId extends AbstractId {
+
+	public TaskId(String value) {
+		super(value);
+	}
+	
+}
