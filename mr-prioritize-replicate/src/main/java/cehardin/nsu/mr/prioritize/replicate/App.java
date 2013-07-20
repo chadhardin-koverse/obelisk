82b265ef11ef8b47f288da38f51a06def681cb81
Chad Hardin <chadhardin@koverse.com>
2013-07-20 13:42:17 -0700

    stuff

--- mr-prioritize-replicate/src/main/java/cehardin/nsu/mr/prioritize/replicate/App.java
+++ mr-prioritize-replicate/src/main/java/cehardin/nsu/mr/prioritize/replicate/App.java
@@ -5,12 +5,14 @@
 package cehardin.nsu.mr.prioritize.replicate;
 
 import static com.google.common.collect.Sets.newHashSet;
+import static com.google.common.collect.Sets.newTreeSet;
 import static com.google.common.collect.Maps.newHashMap;
 import static com.google.common.collect.Multimaps.newMultimap;
-//import static com.google.common.collect.Maps.
+import static com.google.common.collect.Maps.transformValues;
 import static java.lang.String.format;
 import static com.google.common.base.Functions.forMap;
 import static cehardin.nsu.mr.prioritize.replicate.Util.pickRandom;
+import static cehardin.nsu.mr.prioritize.replicate.Util.pickRandomPercentage;
 
 import cehardin.nsu.mr.prioritize.replicate.Variables.Bandwidth;
 import cehardin.nsu.mr.prioritize.replicate.Variables.MapReduceJob;
@@ -19,14 +21,17 @@ import cehardin.nsu.mr.prioritize.replicate.hardware.ClusterBuilder;
 import cehardin.nsu.mr.prioritize.replicate.id.DataBlockId;
 import cehardin.nsu.mr.prioritize.replicate.id.NodeId;
 import cehardin.nsu.mr.prioritize.replicate.id.RackId;
+import cehardin.nsu.mr.prioritize.replicate.id.TaskId;
 import com.google.common.base.Function;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.HashMultimap;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -52,7 +57,8 @@ public class App implements Runnable {
         final int numNodes;
         final int maxConcurrentTasks;
         final int maxTasksPerNode;
-        final SortedSet nodeFailures;
+        final double nodePercentageFailed;
+        final SortedSet<Variables.NodeFailure> nodeFailures;
         final int numRacks;
         final int nodesPerRack;
         final int numDataBlocks;
@@ -60,13 +66,17 @@ public class App implements Runnable {
         final Set<NodeId> nodeIds;
         final Set<DataBlockId> dataBlockIds;
         final Map<NodeId, RackId> nodeToRack;
-        final Multimap<RackId, NodeId> rackToNode;
-        final Multimap<DataBlockId, NodeId> dataBlockToNode;
-        final Function<NodeId, RackId> nodeIdToRackId;
-        final Function<DataBlockId, Set<NodeId>> dataBlockIdToNodeIds;
+        final Multimap<RackId, NodeId> rackToNodes;
+        final Multimap<DataBlockId, NodeId> dataBlockToNodes;
+        final Function<NodeId, RackId> nodeToRackFunction;
+        final Function<DataBlockId, Set<NodeId>> dataBlockToNodesFunction;
         final TaskNodeAllocator taskNodeAllocator;
         final ReplicateTaskScheduler replicateTaskScheduler;
+        final int numTasks;
+        final Set<TaskId> taskIds;
+        final Map<TaskId, DataBlockId> taskToDataBlock;
         final MapReduceJob mapReduceJob;
+        
         final Variables variables;
         
         diskBadwidth = new Bandwidth(100_000_000L, 1, TimeUnit.SECONDS);
@@ -81,23 +91,44 @@ public class App implements Runnable {
         maxTasksPerNode = 2;
         rackIds = newHashSet();
         nodeIds = newHashSet();
-        rackToNode = HashMultimap.create();
-        dataBlockToNode = HashMultimap.create();
+        rackToNodes = HashMultimap.create();
+        dataBlockToNodes = HashMultimap.create();
         nodeToRack = newHashMap();
         dataBlockIds = newHashSet();
+        nodePercentageFailed = 0.25;
+        taskNodeAllocator = new StandardTaskNodeAllocator();
+        replicateTaskScheduler = new StandardReplicateTaskScheduler();
+        numTasks = numNodes * 2;
         
+        taskIds = newHashSet();
+        for(int i=0; i < numTasks; i++) {
+            final TaskId taskId = new TaskId(format("t-%s", i));
+            taskIds.add(taskId);
+        }
+        
+        taskToDataBlock = newHashMap();
+        for(final TaskId taskId : taskIds) {
+            final DataBlockId dataBlockId = pickRandom(random, dataBlockIds);
+            taskToDataBlock.put(taskId, dataBlockId);
+        }
+        
+        mapReduceJob = new MapReduceJob(10, TimeUnit.SECONDS, taskIds, forMap(taskToDataBlock));
         for(int r=1; r <= numRacks; r++) {
             final RackId rackId = new RackId(format("r-%s",1));
             rackIds.add(rackId);
             for(int n=1; n <= nodesPerRack; n++) {
                 final NodeId nodeId = new NodeId(format("r-%s-n-%s", r, n));
                 nodeToRack.put(nodeId, rackId);
-                rackToNode.put(rackId, nodeId);
+                rackToNodes.put(rackId, nodeId);
                 nodeIds.add(nodeId);
             }
         }
         
-        nodeIdToRackId = forMap(nodeToRack);
+//        nodeFailures = newTreeSet(pickRandomPercentage(random, nodeIds, nodePercentageFailed));
+        nodeFailures = newTreeSet();
+        
+        nodeToRackFunction = forMap(nodeToRack);
+        
         for(int d=1; d <= numDataBlocks; d++) {
             final DataBlockId dataBlockId = new DataBlockId(format("d-%s", d));
             dataBlockIds.add(dataBlockId);
@@ -106,16 +137,42 @@ public class App implements Runnable {
         for(final DataBlockId dataBlockId : dataBlockIds) {
             final RackId rack1 = pickRandom(random, rackIds);
             final RackId rack2 = pickRandom(random, rackIds, rack1);
-            final NodeId node1 = pickRandom(random, rackToNode.get(rack1));
-            final NodeId node2 = pickRandom(random, rackToNode.get(rack1), node1);
-            final NodeId node3 = pickRandom(random, rackToNode.get(rack2));
+            final NodeId node1 = pickRandom(random, rackToNodes.get(rack1));
+            final NodeId node2 = pickRandom(random, rackToNodes.get(rack1), node1);
+            final NodeId node3 = pickRandom(random, rackToNodes.get(rack2));
             
-            dataBlockToNode.put(dataBlockId, node1);
-            dataBlockToNode.put(dataBlockId, node2);
-            dataBlockToNode.put(dataBlockId, node3);
-            
+            dataBlockToNodes.put(dataBlockId, node1);
+            dataBlockToNodes.put(dataBlockId, node2);
+            dataBlockToNodes.put(dataBlockId, node3);
         }
-        variables = new Variables
+        
+        dataBlockToNodesFunction = forMap(transformValues(dataBlockToNodes.asMap(), new Function<Collection<NodeId>, Set<NodeId>>() {
+            @Override
+            public Set<NodeId> apply(Collection<NodeId> nodeIds) {
+                return newHashSet(nodeIds);
+            }            
+        }));
+        
+        variables = new Variables(
+                diskBadwidth, 
+                rackBandwidth, 
+                clusterBandwidth, 
+                blockSize, 
+                maxConcurrentTasks, 
+                maxTasksPerNode, 
+                nodeFailures, 
+                rackIds, 
+                nodeIds, 
+                dataBlockIds, 
+                nodeToRackFunction, 
+                dataBlockToNodesFunction, 
+                taskNodeAllocator, 
+                replicateTaskScheduler, 
+                mapReduceJob);
+        
+        final Simulator simulator = new Simulator(variables, Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
+        
+        simulator.call();
     }
 }
 
--- mr-prioritize-replicate/src/main/java/cehardin/nsu/mr/prioritize/replicate/Util.java
+++ mr-prioritize-replicate/src/main/java/cehardin/nsu/mr/prioritize/replicate/Util.java
@@ -5,6 +5,7 @@ import static com.google.common.collect.Iterables.get;
 import static com.google.common.collect.Iterables.contains;
 import static com.google.common.collect.Lists.newArrayList;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Random;
 
 /**
@@ -27,6 +28,20 @@ public class Util {
             final int index = random.nextInt(size(items));
             chosen = get(items, index);
         } while(contains(not, chosen));
+        
+        return chosen;
+    }
+    
+    public static <T> Iterable<T> pickRandomPercentage(final Random random, final Collection<T> items, final double percentage) {
+        return pickRandom(random, items, (int)(items.size() * percentage));
+    }
+    
+    public static <T> Iterable<T> pickRandom(final Random random, final Iterable<T> items, final int amount) {
+        final Collection<T> chosen = newArrayList();
+        
+        while(chosen.size() < amount) {
+            chosen.add(pickRandom(random, items, chosen));
+        }
         
         return chosen;
     }
